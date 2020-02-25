85088fdeb1a393c277a25ee99e227bf77253e4c8
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-25 15:06:15 -0500

    Feat - add css to datoCMS data inside home page

--- src/components/homeContent.js
+++ src/components/homeContent.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import {css} from '@emotion/core';
+
+const HomeText = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 5rem;
+    }
+
+    p {
+        line-height: 2;
+    }
+`;
 
 const HomeContent = () => {
 
@@ -23,11 +43,24 @@ const HomeContent = () => {
         }
     `);
 
-    console.log(info)
+    const { title, content, image } = info.allDatoCmsPage.nodes[0];
 
     return (
         <>
-            <h2>Page title</h2>
+            <h2
+                css={
+                    css`
+                        text-align: center;
+                        font-size: 4rem;
+                        margin-top: 4rem;
+                    `
+                }
+            >{title}</h2>
+
+            <HomeText>
+                <p>{content}</p>
+                <Image fluid={image.fluid}/>
+            </HomeText>
         </>
     );
 }
