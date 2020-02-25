5a6ddab675febc6942230257df095b3de453a251
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-25 07:46:09 -0500

    Feat - add css styles into Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,41 @@
 import React from 'react';
+import { css } from '@emotion/core';
+
 const Header = () => {
-    return ( <h1>From the header</h1> );
+    return (
+        <header
+            css={
+                css`
+                    background-color: #333;
+                    padding: 1rem;
+                `
+            }
+        >
+            <div
+                css={
+                    css`
+                        margin: 0 auto;
+                        max-width: 1200px;
+
+                        @media (min-width: 768px) {
+                            display: flex;
+                            align-items: center;
+                            justify-content: space-between;
+                        }
+                    `
+                }
+            >
+                <h1
+                    css={
+                        css`
+                            color: #FFF;
+                            text-align: center;
+                        `
+                    }
+                >Hotel Gatsby</h1>
+            </div>
+        </header>
+    );
 }
  
 export default Header;
