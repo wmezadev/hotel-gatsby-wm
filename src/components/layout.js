b4d31df963c67901889ebc70e121bc5b4f15609b
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-25 07:39:55 -0500

    Feat - add @emotion global styles into Layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import Header from './header';
 import Helmet from 'react-helmet';
-import './layout.css';
+import { Global, css } from '@emotion/core';
+
 
 const Layout = (props) => {
     return ( 
-        <> 
+        <>
+            <Global
+                styles={css`
+                    html {
+                        font-size: 62.5%;
+                    }
+                    body {
+                        font-size: 16px;
+                        font-size: 1.6rem;
+                        line-height: 1.5;
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', sans-serif;
+                    }
+                    h3 {
+                        font-family: 'PT Sanas', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                `}
+            />
             <Helmet>
                 <title>Gatsby Hotel</title>
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" rel="stylesheet"/>
                 <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet"/>
             </Helmet>
             <Header/>
