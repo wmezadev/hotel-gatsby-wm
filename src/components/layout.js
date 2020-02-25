39b76ab47be0baf296d42665c29ba3da686ce6e7
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-24 20:02:32 -0500

    Feat - add firts layour

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './header';
+import Helmet from 'react-helmet';
+import './layout.css';
 
 const Layout = (props) => {
     return ( 
         <> 
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet"/>
+            </Helmet>
             <Header/>
             {props.children} 
         </>
