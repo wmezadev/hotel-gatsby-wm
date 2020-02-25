552363cd8ea328782d945bd462118509a593c4b9
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-25 15:19:33 -0500

    Feat - add styles to about us page

--- src/components/aboutContent.js
+++ src/components/aboutContent.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const Content = styled.main`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px) {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 2rem;
+    }
+
+    p {
+        line-height: 2;
+        margin-top: 2rem;
+    }
+`;
 
 const AboutContent = () => {
 
@@ -24,13 +44,19 @@ const AboutContent = () => {
 
     return (
         <>
-            <h2>{title}</h2>
-            <div>
+            <h2
+                css={css`
+                    margin-top: 4rem;
+                    text-align: center;
+                    font-size: 4rem;
+                `}
+            >{title}</h2>
+            <Content>
                 <p>{content}</p>
                 <Image
                     fluid={image.fluid}
                 />
-            </div>
+            </Content>
         </>
     );
 }
