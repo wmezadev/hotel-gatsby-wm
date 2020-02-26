7e141775660fce200e6369d907bcfda42cfb01d9
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-26 09:27:32 -0500

    Feat - create rooms hooks to fetch data from DatoCMS

---
+++ src/components/previewRoom.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PreviewRoom = () => {
+    return ( <p>From room</p> );
+}
+ 
+export default PreviewRoom;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,38 @@ import React from "react"
 import Layout from '../components/layout';
 import ImageHotel from '../components/imageHotel';
 import HomeContent from '../components/homeContent';
+import useRooms from '../hooks/useRooms';
+import { css } from '@emotion/core';
+import PreviewRoom from '../components/previewRoom';
 
-const IndexPage = () => (
-  <Layout>
-    <ImageHotel/>
-    <HomeContent/>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const rooms = useRooms();
+
+  console.log(rooms);
+
+  return (
+    <Layout>
+      <ImageHotel/>
+      <HomeContent/>
+      <h2
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+          font-size: 3rem;
+        `}
+      >
+        Our rooms
+      </h2>
+      <ul>
+        {rooms.map(room => (
+          <PreviewRoom
+            
+          />
+        ))}
+      </ul>
+    </Layout>
+  )
+}
 
 export default IndexPage
