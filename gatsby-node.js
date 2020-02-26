ba31007b73b1f25d8b579de9a06cac85f6cedade
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-26 09:57:54 -0500

    Feat - add dynamically pages (rooms) using DatoCMS and gatsby-node createpage

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,32 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const result = await graphql(`
+        query {
+            allDatoCmsRoom{
+                nodes {
+                    slug
+                }
+            }
+        }    
+    `);
+
+    // if error 
+    if(result.error) {
+        reporter.panic('There is no result', result.errors);
+    }
+
+    // if no errors on fetching data
+    const rooms = result.data.allDatoCmsRoom.nodes;
+
+    rooms.forEach(room => {
+        actions.createPage({
+            path: room.slug,
+            component: require.resolve('./src/components/rooms.js'),
+            context: {
+                slug: room.slug
+            }
+        });
+    });
+}
---
+++ src/components/rooms.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RoomTemplate = () => {
+    return ( 
+        <h1>Room.js</h1>
+    );
+}
+ 
+export default RoomTemplate;
