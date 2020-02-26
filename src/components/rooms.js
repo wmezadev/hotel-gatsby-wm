8c0f14aeaf28cc4f4f73771157aa5c3e98b1eb50
William Eduardo Meza <williameduardomeza@gmail.com>
2020-02-26 10:25:06 -0500

    Feat - query with graphql room data using slug

--- src/components/rooms.js
+++ src/components/rooms.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-const RoomTemplate = () => {
+export const query = graphql`
+    query ($slug: String!) {
+        allDatoCmsRoom(filter: { slug: { eq: $slug }}) {
+            nodes {
+                title
+                content
+                image {
+                    fluid(maxWidth: 1200) {
+                        ...GatsbyDatoCmsFluid
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const RoomTemplate = ({ data }) => {
+
+    console.log(data);
+    
     return ( 
         <h1>Room.js</h1>
     );
